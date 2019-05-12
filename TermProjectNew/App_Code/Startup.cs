89723bec944f3a579e5ced5a60013d7761b40696
baasakc <baasakc@gmail.com>
2019-05-12 12:10:13 +0300

    Add project files.

---
+++ TermProjectNew/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TermProjectNew.Startup))]
+namespace TermProjectNew
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
